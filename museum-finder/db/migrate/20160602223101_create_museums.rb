bf69efb7ebb5637674503d9997f1889bcae3fd6b
Katherine Broner <katherine.broner@gmail.com>
2016-06-02 18:52:31 -0400

    Update museums migration
    Changes made: 
    - Adjust input type for lat and lng

--- museum-finder/db/migrate/20160602223101_create_museums.rb
+++ museum-finder/db/migrate/20160602223101_create_museums.rb
@@ -2,13 +2,13 @@ class CreateMuseums < ActiveRecord::Migration
   def change
     create_table :museums do |t|
       t.string :name, null: false
-      t.text :formatted_address, null: false
+      t.string :formatted_address
       t.string :formatted_phone_number
-      t.string :lat
-      t.string :ing
-      t.text :opening_hours
+      t.decimal :lat
+      t.decimal :lng
+      t.string :opening_hours
       t.string :photos
-      t.string :place_ids
+      t.string :place_id
       t.string :rates
       t.string :is_free
       t.references :category, index: true
