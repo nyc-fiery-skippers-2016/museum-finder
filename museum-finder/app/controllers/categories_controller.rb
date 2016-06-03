e9a7b830f6b14f74b8e958c0c03c64ac16dce17c
Brian Donahue <bpd4tw@virginia.edu>
2016-06-03 15:57:05 -0400

    Add controller for museums and categories. Add category listing html for category index

---
+++ museum-finder/app/controllers/categories_controller.rb
@@ -0,0 +1,10 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+  end
+
+end
---
+++ museum-finder/app/controllers/museums_controller.rb
@@ -0,0 +1,7 @@
+class MuseumsController < ApplicationController
+
+  def show
+    @film = Film.find_by(id: params[:id])
+  end
+
+end
