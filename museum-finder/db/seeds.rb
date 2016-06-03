9ed06df65dec6822f9616e678c1d621467324b1f
Katherine Broner <katherine.broner@gmail.com>
2016-06-03 17:13:59 -0400

    Clean API data with specific keys and combine with rates from NYCGo API to instantiate Museum objects

--- museum-finder/db/seeds.rb
+++ museum-finder/db/seeds.rb
@@ -1,3 +1,5 @@
+Museum.delete_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -11,26 +13,46 @@ converted_data = Crack::XML.parse(data)
 
 museums = converted_data['museums']['museum']
 
-# museum_index = museums.each do |museum|
-#   Museum.create!( name: museum['name'] )
-# end
+
 
 @client = GooglePlaces::Client.new("AIzaSyAuZ_wtI3N7LcecgNJ_sJVq_Aun5WB9W10")
 
-queried_museums = []
-museums.map do |museum|
-  queried_museums << @client.spots_by_query(museum["name"])
+queried_museums = museums.map do |museum|
+  @client.spots_by_query(museum["name"])
 end
 
-full_museum_data = []
-queried_museums.each do |museum|
-  full_museum_data << @client.spot(museum.first.place_id)
+full_museum_data = queried_museums.map do |museum|
+  @client.spot(museum.first.place_id)
 end
 
+cleaned_museum_data = full_museum_data.map do |museum|
+  museum_hash = {}
+  museum_hash[:name] = museum.name
+  museum_hash[:formatted_address] = museum.formatted_address
+  museum_hash[:formatted_phone_number] = museum.formatted_phone_number
+  museum_hash[:lat] = museum.lat
+  museum_hash[:lng] = museum.lng
+  museum_hash[:opening_hours] = museum.opening_hours
+  museum_hash[:photos] = museum.photos
+  museum_hash[:place_id] = museum.place_id
+  museum_hash
+end
 
-# full_museum_data.each do |musem|
-#   museum.slice!("name", "formatted_address", "formatted_phone_number", "lat", "lng", "opening hours", "photos", "place_id")
-# end
+cleaned_museum_data.each_with_index do |museum, idx|
+  Museum.create!(
+    name: museum[:name],
+    formatted_address: museum[:formatted_address],
+    formatted_phone_number: museum[:formatted_phone_number],
+    lat: museum[:lat],
+    lng: museum[:lng],
+    opening_hours: museum[:opening_hours],
+    photos: museum[:photos],
+    place_id: museum[:place_id],
+    rates: museums[idx]["rates"])
+end
 
-# end
+Museum.find(1,4,7).update_all(category: "Modern")
+
+
+
 
