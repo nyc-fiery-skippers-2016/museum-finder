7111c5f65930a951b427f041846211f9c92bb269
sergener <sibel.ergener@gmail.com>
2016-06-04 20:35:41 -0400

    Complete ability to unfavorite

--- museum-finder/app/controllers/favorites_controller.rb
+++ museum-finder/app/controllers/favorites_controller.rb
@@ -1,22 +1,13 @@
 class FavoritesController < ApplicationController
 
   def create
-  end
-
-  def destroy
-  end
-
-
-
-  def create
-   @project = Project.find_by(id: params[:favorite][:project_id])
-   favorite = Favorite.find_or_create_by(user:current_user, project: @project)
-    redirect_to projects_path
+   favorite = Favorite.find_or_create_by(user:current_user, museum: museum)
+    redirect_to categories_path
   end
 
     def destroy
     Favorite.find_by(id: params[:id]).destroy
-    redirect_to projects_path
+    redirect_to categories_path
   end
 
 end
