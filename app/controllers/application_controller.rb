22a5a31084fef941c3c3c14cca3f49d910a228e8
Katherine Broner <katherine.broner@gmail.com>
2016-06-07 10:48:51 -0400

    Begin basic styling for home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+  end
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected	
+  protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :monthly_email) }
