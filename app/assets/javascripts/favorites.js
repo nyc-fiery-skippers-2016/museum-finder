b21960c065b578e9d941e27ba852f52a0e727cee
sergener <sibel.ergener@gmail.com>
2016-06-07 09:10:58 -0400

    BAsic favoriting ajax

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -1,10 +1,24 @@
 // $(document).ready(function() {
-//   $('#new_favorite').on('submit', function(e) {
+//   $('.filled-star').on('submit', function(e) {
 //     e.preventDefault();
 //     debugger;
 //     $.ajax({
 //     }).done(function(response) {
+//     	$('.filled-star').remove();
+//     	// parent.append($(".unfilled-star"))
 //     });
 //  });
 // });
   
+
+// $(document).ready(function() {
+//   $('.unfilled-star').on('submit', function(e) {
+//     e.preventDefault();
+//     debugger;
+//     $.ajax({
+//     }).done(function(response) {
+//     	$('.unfilled-star').remove();
+//     	// parent.append($(".filled-star"))
+//     });
+//  });
+// });
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,15 @@ class FavoritesController < ApplicationController
   def create
   	museum = Museum.find_by(id: params[:favorite][:museum_id])
    favorite = Favorite.find_or_create_by(user:current_user, museum: museum)
-    render plain: "Yay created"
+    if favorite.save
+      if request.xhr?
+        render partial: 'favorite', layout: false
+      else
+        redirect_to :back
+      end
+    else
+      render plain: "That didn't work"
+    end
   end
 
    def destroy
@@ -11,7 +19,7 @@ class FavoritesController < ApplicationController
    	if fave
    		fave.destroy
    		if request.xhr?
-   			render json: {success: true}
+   			render 
    		else
    			redirect_to :back
    		end
