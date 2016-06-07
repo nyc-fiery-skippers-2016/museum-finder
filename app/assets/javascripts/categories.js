bb9f86ce1a0eaf97ebbfd32ea6f795cabb133fde
Katherine Broner <katherine.broner@gmail.com>
2016-06-07 16:26:17 -0400

    Make CSS changes to category home

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,17 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).ready(function(){
-  $(".category").hide();
-  $("#art-category").show();
-  $(".padded-nav").on("click", "a", function(e) {
-    e.preventDefault();
-    var tab = $(this).attr("id");
+// $(document).ready(function(){
+//   $(".category").hide();
+//   $("#art-category").show();
+//   $(".padded-nav").on("click", "a", function(e) {
+//     e.preventDefault();
+//     var tab = $(this).attr("id");
 
-    $(this).parent().parent().children().removeClass("active");
-    $(this).parent().addClass("active");
+//     $(this).parent().parent().children().removeClass("active");
+//     $(this).parent().addClass("active");
 
-    $(".category").hide();
-    $("#" + tab + "-category").show();
-  })
-  });
+//     $(".category").hide();
+//     $("#" + tab + "-category").show();
+//   })
+//   });
