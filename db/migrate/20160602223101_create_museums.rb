19491407c3e7e17e78ffd468a7004f9b8cee80ae
Brian Donahue <bpd4tw@virginia.edu>
2016-06-08 14:48:45 -0400

    Add images back into museum card partial

--- db/migrate/20160602223101_create_museums.rb
+++ db/migrate/20160602223101_create_museums.rb
@@ -7,7 +7,7 @@ class CreateMuseums < ActiveRecord::Migration
       t.decimal :lat
       t.decimal :lng
       t.string :opening_hours
-      t.string :photo
+      t.string :photos
       t.string :place_id
       t.string :rates
       t.string :is_free
