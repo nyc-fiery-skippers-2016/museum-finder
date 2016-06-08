19956463025abf73f45490995617cc47c2a2cd3d
sergener <sibel.ergener@gmail.com>
2016-06-08 17:31:05 -0400

    Change search bar for added simplicity

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-    if jump = jump_target
-      redirect_to jump
-    else
-      @keyword = params[:keyword]
-      @results = Search.for(params[:keyword])
-    end
+    @keyword = params[:keyword]
+    @results = Search.for(params[:keyword])
   end
-
-  private
-    def jump_target
-      Museum.find_by(name: params[:keyword])
-    end
+    
 end
