199dd6b960ab5048a7ffb57c7ba2892a97c14149
sergener <sibel.ergener@gmail.com>
2016-06-08 15:49:16 -0400

    Fix footer to not cover body and add search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
     if jump = jump_target
       redirect_to jump
     else
+      @keyword = params[:keyword]
       @results = Search.for(params[:keyword]) && Search.for_address(params[:keyword])
       @favorite = Favorite.new
     end
